1dbb127711852b076aba38ab57a47825676baf8e
Aldin <aldin.bijedic@gmx.at>
2022-01-03 01:25:41 +0100

    edited some files

--- script/script.js
+++ script/script.js
@@ -1,93 +1,124 @@
 let fields = [];
 let tableData = document.querySelectorAll('td');
-let currentPlayer = 'circle';
-let gameActive = true;
+let currentPlayer = 'cross';
 let gameTail = false;
+let gameOver = false;
 
-function draw(position) {
-     currentPlayer = (fields[position] == null) ? fields[position] = currentPlayer : fields[position];
-    if (gameActive) {
-        if (currentPlayer == 'circle') {
-            currentPlayer = 'cross';
-            tableData[position].classList.add('circle');
-        } else {
-            currentPlayer = 'circle';
-            tableData[position].classList.add('cross');
+function addShape(id) {
+    if (!fields[id] && !gameOver) {
+        if (!fields[id]) {
+            fields[id] = currentPlayer;
+            (currentPlayer == 'cross') ? currentPlayer = 'circle' : currentPlayer = 'cross';
+            changePlayerSlider();
+            checkWinner();
+            draw();
+            showRestartButton();
         }
-        changePlayerSlider();
-        checkWinner();
     }
-    showRestartButton();
+}
+
+function draw() {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'cross') {
+            tableData[i].classList.add('cross', 'bounceIn');
+        }
+        if (fields[i] == 'circle') {
+            tableData[i].classList.add('circle', 'bounceIn');
+        }
+    }
 }
 
 function checkWinner() {
-
+    let winner;
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
-        console.log('Gewonnen: ' + fields[0]);
-        gameActive = false;
+        winner = fields[0];
+        document.getElementById('line-3').style.transform = "scaleX(1)";
     }
 
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
-        console.log('Gewonnen: ' + fields[3]);
-        gameActive = false;
-
+        winner = fields[3];
+        document.getElementById('line-1').style.transform = "scaleX(1)";
     }
 
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
-        console.log('Gewonnen: ' + fields[6]);
-        gameActive = false;
+        winner = fields[6];
+        document.getElementById('line-2').style.transform = "scaleX(1)";
+
     }
 
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         console.log('Gewonnen: ' + fields[0]);
-        gameActive = false;
+        winner = fields[0];
+        document.getElementById('line-4').style.transform = "rotate(90deg) scaleX(1)";
+
     }
 
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         console.log('Gewonnen: ' + fields[1]);
-        gameActive = false;
+        winner = fields[1];
+        document.getElementById('line-5').style.transform = "rotate(90deg) scaleX(1)";
     }
 
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         console.log('Gewonnen: ' + fields[2]);
-        gameActive = false;
+        winner = fields[2];
+        document.getElementById('line-6').style.transform = "rotate(90deg) scaleX(1)";
+
     }
 
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         console.log('Gewonnen: ' + fields[0]);
-        gameActive = false;
+        winner = fields[0];
+        document.getElementById('line-7').style.transform = "rotate(45deg) scaleX(1.1)";
     }
 
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         console.log('Gewonnen: ' + fields[2]);
-        gameActive = false;
+        winner = fields[2];
+        document.getElementById('line-8').style.transform = "rotate(-45deg) scaleX(1.1)";
+
     }
-    isGameTail();
-    console.log(gameTail);
+    checkIfTie();
+    if (winner) {
+        gameOver = true;
+        showRestartButton();
+    }
+
 }
 
-function isGameTail() {
-    gameTail = (fields.filter(String).length == 9) ? true : false;
+function checkIfTie() {
+    return gameTail = (fields.filter(String).length == 9) ? true : false;
+
 }
 
 function showRestartButton() {
-    if (!gameActive) {
+    if (gameOver) {
         document.getElementById('restart').classList.remove('d-none');
     }
 }
 
 function restartGame() {
     fields = [];
-    gameActive = true;
+    gameOver = false;
     currentPlayer = 'circle';
-    for (let i = 0; i < tableData.length; i++) {
-        tableData[i].classList.remove('circle', 'cross');
-    }
+    removeSpapes();
     changePlayerSlider();
+    removeAttributes();
     document.getElementById('restart').classList.add('d-none');
 }
 
+function removeSpapes(){
+    for (let i = 0; i < tableData.length; i++) {
+        tableData[i].classList.remove('circle', 'cross');
+    }
+}
 
+function removeAttributes(){
+    let lines = document.querySelectorAll('[id*="line-"]');
+    for (let i = 0; i < lines.length; i++) {
+        lines[i].removeAttribute('style');
+    }
+}
 
 function changePlayerSlider() {
     let playerCross = document.getElementById('player-cross');
